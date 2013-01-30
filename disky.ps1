c2bfe947050229b3f0972a4079c57ed55c2bfc84
gen0cide- <gen0cide.threats@gmail.com>
2013-01-29 18:47:25 -0800

    Code cleanup

--- disky.ps1
+++ disky.ps1
@@ -1,31 +1,38 @@
-$drives = get-wmiobject win32_diskdrive
-
-$emailfrom = "DriveWatch"
-$emailto = ""
-$subject = "ERROR!"
-$body = ""
+# ---------------------------------------------------------------------------------------
+$drives     = get-wmiobject win32_diskdrive
+$emailfrom  = "DriveWatch"
+$emailto    = ""
+$subject    = "ERROR!"
+$body       = ""
 $SMTPServer = "smtp.gmail.com"
 $SMTPClient = New-Object Net.Mail.SmtpClient($SMTPServer, 587)
-$SMTPClient.EnableSsl = $true
+# ---------------------------------------------------------------------------------------
+$SMTPClient.EnableSsl   = $true
 $SMTPClient.Credentials = New-Object System.Net.NetworkCredential($username, $password)
+# ---------------------------------------------------------------------------------------
 $error = $false
-
-foreach($drive in $drives) {
-	if($drive.status -ne "OK") {
-		$error = $true
-		$partitions = $drive.getrelated("Win32_DiskPartition")
-		$body = "The following partitions have drives that are not ok: `n`n"
-		
-		foreach($partition in $partitions) {
-			$letter = $partition.getrelated("Win32_LogicalDisk")
-			foreach($l in $letter) { #stupid workaround because getrelated returns an object collectio			
-				$body += "`t" + $l.caption
-			}
-		}
-	}
-	$body += "`n"
+# ---------------------------------------------------------------------------------------
+foreach($drive in $drives) 
+{
+  if($drive.status -ne "OK") 
+  {
+    $error      = $true
+    $partitions = $drive.getrelated("Win32_DiskPartition")
+    $body       = "The following partitions have drives that are not ok: `n`n"		
+    foreach($partition in $partitions) 
+    {
+      $letter = $partition.getrelated("Win32_LogicalDisk")
+      foreach($l in $letter) 
+      { 
+        $body += "`t" + $l.caption
+      }
+    }
+  }
+  $body += "`n"
 }
-
-if($error) {
-	$SMTPClient.Send($EmailFrom, $EmailTo, $Subject, $Body)
+# ---------------------------------------------------------------------------------------
+if($error) 
+{
+  $SMTPClient.Send($EmailFrom, $EmailTo, $Subject, $Body)
 }
+# ---------------------------------------------------------------------------------------
