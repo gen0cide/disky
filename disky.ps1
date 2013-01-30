e803776269f25362b9a98672a7ba1099e981104a
Connor Kelly <connor.r.kelly@gmail.com>
2013-01-29 21:01:20 -0500

    Initial Script

---
+++ disky.ps1
@@ -0,0 +1,31 @@
+$drives = get-wmiobject win32_diskdrive
+
+$emailfrom = "DriveWatch"
+$emailto = ""
+$subject = "ERROR!"
+$body = ""
+$SMTPServer = "smtp.gmail.com"
+$SMTPClient = New-Object Net.Mail.SmtpClient($SMTPServer, 587)
+$SMTPClient.EnableSsl = $true
+$SMTPClient.Credentials = New-Object System.Net.NetworkCredential($username, $password)
+$error = $false
+
+foreach($drive in $drives) {
+	if($drive.status -ne "OK") {
+		$error = $true
+		$partitions = $drive.getrelated("Win32_DiskPartition")
+		$body = "The following partitions have drives that are not ok: `n`n"
+		
+		foreach($partition in $partitions) {
+			$letter = $partition.getrelated("Win32_LogicalDisk")
+			foreach($l in $letter) { #stupid workaround because getrelated returns an object collectio			
+				$body += "`t" + $l.caption
+			}
+		}
+	}
+	$body += "`n"
+}
+
+if($error) {
+	$SMTPClient.Send($EmailFrom, $EmailTo, $Subject, $Body)
+}
